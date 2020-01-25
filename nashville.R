cc469c5a360d8d87a924b0f90b81c7781fe15429
Owen <octhompson19@gmail.com>
2020-01-25 17:22:36 -0600

    nashville map

--- nashville.R
+++ nashville.R
@@ -1,19 +1,29 @@
 # Nashville ####
+library(osmdata)
+library(tidyverse)
 nash_streets <- getbb("Nashville TN") %>% 
   opq() %>% 
-  add_osm_feature(key = "highway") %>% 
+  add_osm_feature(key = "highway", 
+                  value = c("motorway", "primary", 
+                            "secondary", "tertiary")) %>% 
   osmdata_sf()
 
 nash_small_streets <- getbb("Nashville TN") %>% 
   opq()%>%
   add_osm_feature(key = "highway", 
-                  value = c("road")) %>%
+                  value = c("road", "cycleway", "living_street", "footway")) %>%
   osmdata_sf()
 
 nash_river <- getbb("Nashville TN") %>% 
   opq()%>%
   add_osm_feature(key = "waterway", value = "river") %>%
   osmdata_sf()
+
+water <- getbb("Nashville TN") %>%
+  opq()%>%
+  add_osm_feature(key = "natural", value = "water") %>%
+  osmdata_sf()
+
 
 ggplot() +
   geom_sf(data = nash_streets$osm_lines,
@@ -72,4 +82,40 @@ ggplot() +
            expand = FALSE) + 
   theme_void() + 
   theme(plot.background = element_rect(fill = "#282828"))
+
   
+  
+  
+  
+ggplot() +
+  geom_sf(data = nash_streets$osm_lines,
+          inherit.aes = FALSE,
+          color = "black",
+          size = .4,
+          alpha = .8) +
+  geom_sf(data = nash_small_streets$osm_lines,
+          inherit.aes = FALSE,
+          color = "black",
+          size = .4,
+          alpha = .6) +
+  geom_sf(data = nash_river$osm_lines,
+          inherit.aes = FALSE,
+          color = "black",
+          size = .4,
+          alpha = .5) + 
+  # geom_sf(data = water$osm_multipolygons, 
+  #         inherit.aes = F, 
+  #         color = "black") + 
+  coord_sf(xlim = c(-86.95, -86.65), 
+           ylim = c(36.07, 36.26),
+           expand = FALSE) + 
+  labs(caption = "NASHVILLE") + 
+  theme_void() + 
+  theme(legend.position = "none",
+        plot.caption = element_text(color = "grey20", 
+                                    size = 70, 
+                                    hjust = .5, 
+                                    face = "plain", 
+                                    family = "Didot")
+  )
+  theme(plot.background = element_rect(fill = "#282828"))
